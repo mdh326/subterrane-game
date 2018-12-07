98a0c4057738aad88e7b9871abd639b5d52621c4
MarkH-PC\MarkH <mdh326@drexel.edu>
2018-12-06 22:33:19 -0500

    crystal glow 0.5

---
+++ subterrance-project/Assets/crystalGlowScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crystalGlowScript : MonoBehaviour {
+
+    public float pulseTime = 4f;
+    private Material myMat;
+    private Light myLight;
+    private float myTimer = 0;
+    private float sineTime = 0;
+    private float glowFactor;
+
+	// Use this for initialization
+	void Start () {
+        myMat = gameObject.GetComponent<Renderer>().material;
+        myLight = gameObject.GetComponentInChildren<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myTimer += Time.deltaTime;
+
+        if (myTimer > pulseTime / 2){
+            sineTime += Time.deltaTime;
+        } else if (myTimer < pulseTime / 2){
+            sineTime -= Time.deltaTime;
+        }
+
+        if(sineTime > 1f){
+            sineTime = 1f;
+        } else if (sineTime < 0f){
+            sineTime = 0f;
+        }
+
+        glowFactor = 0.1f + 0.9f * ();
+	}
+}
